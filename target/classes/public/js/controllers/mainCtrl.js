f65aa4f8c467fc76812c134c0e43dc7392b86484
Ananasy <anastasiya_johnni@mail.ru>
2017-09-02 19:32:12 +0300

    try to fix untracked files

---
+++ target/classes/public/js/controllers/mainCtrl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ananasy on 15.03.2017.
+ */
+angular.module('app.controllers', ['app.services'])
+    .controller('mainCtrl', function($scope){
+        $scope.answer = {
+            mlp: "",
+            cnn: "",
+            cnn_exp: ""
+        }
+    });
